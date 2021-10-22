d26ae896a35677e327435079fd3bda02be8978c3
Dualock <djfonsecamo@gmail.com>
2021-10-21 22:18:25 -0600

    Relojes

--- Avance1/Relojes/probador.v
+++ Avance1/Relojes/probador.v
@@ -2,31 +2,34 @@ module probador(
 
 //------------- outputs----------
 	output reg a,
-	output reg clk32f,
+	output reg clk32f,rst,
 //-------------- inputs ---------
-	input clk,clk2f,clk4f,
+	input clk_out_c,clk2f_out_c,clk4f_out_c, //Inputs de modulo conductual
+	input clk_out_e, clk2f_out_e, clk4f_out_e, //Inputs de modulo estructural
 	input not_a
 );
 
 	initial begin
 		$dumpfile("resultados.vcd");
 		$dumpvars;
-			$display ("\t\t\tclk32f \tclk4f \tclk2f \tclk_test \tnot_a");
+			$display ("\t\t\tclk32f \tclk4f \tclk2f \tclk \tnot_a");
 			
-			$monitor($time,"\t%b\t%b\t%b\t%b \t%b" ,clk32f, clk4f, clk2f, clk, not_a);	
+			$monitor($time,"\t%b\t%b\t%b\t%b \t%b" ,clk32f, clk4f_out_c, clk2f_out_c, clk_out_c, not_a);	
 			{a} = 'b0;
-			repeat (12) begin		
-				@(posedge clk);
+			repeat (6) begin		
+				@(posedge clk4f_out_c);
 				{a} <= {a}+1;	
 			end
-			@(posedge clk);	
+			@(posedge clk4f_out_c);	
 			a <= 0;
 			$finish;
 	end
 initial	a <= 5;
+initial rst <= 1;
+always #4 rst <= 0;
 initial clk32f <= 0;
 
-always #5 clk32f <= ~clk32f;
+always #2 clk32f <= ~clk32f;
 
 
 endmodule
